9e1ea0bc377950f43608bb7f4245853da07ed0ef
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-25 01:26:10 +0700

    feat: Add ESLint disable unwanted warning and error
    feat: Add server init async funciton

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
+/* eslint-disable eol-last */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
 console.log('Server is running!');
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server is running on Port: ${server.info.uri}`);
+};
+
+init();
