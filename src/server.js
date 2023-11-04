3bf4a6db00939a9ff4f77d2e5f07788756db6fe1
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-11-04 11:39:09 +0700

    set the host and cors to be compatible with dev prod environment

--- src/server.js
+++ src/server.js
@@ -9,10 +9,10 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin: ['http://notesapp-v1.dicodingacademy.com'],
+        origin: ['*'],
       },
     },
   });
